81e216eafdf661d757cd2fe965711f175cc2a41c
yewei600 <ericwei94@gmail.com>
2017-10-04 13:54:18 -0400

    first commit

---
+++ index.ios.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text, AppRegistry, View } from "react-native";
+import Header from "./src/components/header";
+import AlbumList from "./src/components/AlbumList";
+
+const App = () => {
+  return (
+    <View>
+      <Header headerText={"albums"} />
+      <AlbumList />
+    </View>
+  );
+};
+
+AppRegistry.registerComponent("albums", () => App);
---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const AlbumDetail = props => {
+  return (
+    <View>
+      <Text>{props.album.title}</Text>
+    </View>
+  );
+};
